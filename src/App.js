8cb49d530ce7a059ef54629113a237436e5f5e8c
Noah Peden <noahpeden@gmail.com>
2017-04-13 13:05:03 -0600

    garage opens and closes with no animation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Reveal from 'react-reveal';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 import 'animate.css/animate.css';
 
@@ -9,22 +10,40 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-
+      items: null,
     }
   }
   openGarage(){
     axios
-    .get('')
+    .get('https://garage-bin-be.herokuapp.com/api/v1/items')
+    .then((response) => {
+      this.setState({
+        items: response.data
+      })
+    })
+  }
+
+  closeGarage(){
+    this.setState({
+      items: null,
+    })
   }
 
   render() {
+    let items = this.state.items
     return (
         <div className="App">
           <h1>Garage Bin - Check What Items Lie in the Beyond!</h1>
-          <button>Open!</button>
-          <button>Close!</button>
+          <button onClick={() => this.openGarage()}>Open!</button>
+          <button onClick={() => this.closeGarage()}>Close!</button>
           <div>
-
+            {items ? items.map((item)=> {
+              return <ul>
+                <li>Name: {item.name}</li>
+                <li>Reason: {item.reason}</li>
+                <li>cleanliness: {item.cleanliness}</li>
+              </ul>
+            }) : <div>Door is closed</div>}
           </div>
         </div>
       );
