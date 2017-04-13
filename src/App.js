2f6ba8e89f725534e4c8e3f4b22689d65724167f
Noah Peden <noahpeden@gmail.com>
2017-04-13 12:15:28 -0600

    testing 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-hello world!!!!!
-        </p>
+        <h1>This is a Garage of Crazy Items!</h1>
       </div>
     );
   }
