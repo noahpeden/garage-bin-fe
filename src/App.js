b374e2fa09e49ba782dbf2e12f9f1ec66858090b
Noah Peden <noahpeden@gmail.com>
2017-04-13 12:43:03 -0600

    starting garage open

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import Reveal from 'react-reveal';
+import logo from './logo.svg';
+import './App.css';
+import 'animate.css/animate.css';
 
 class App extends Component {
   constructor(){
@@ -8,13 +12,24 @@ class App extends Component {
 
     }
   }
+  openGarage(){
+    axios
+    .get('')
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>This is a Garage of Crazy Items!</h1>
-      </div>
-    );
+        <div className="App">
+          <h1>Garage Bin - Check What Items Lie in the Beyond!</h1>
+          <button>Open!</button>
+          <button>Close!</button>
+          <div>
+
+          </div>
+        </div>
+      );
+    }
   }
-}
+
 
 export default App;
