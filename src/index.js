0b899171e22a001c43b745af242dc15f6731b72f
Noah Peden <noahpeden@gmail.com>
2017-04-13 18:20:38 -0600

    single item view very buggy, but can patch cleanliness

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import AddItem from './addItem';
+import SingleItem from './SingleItem'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+  <Router history={browserHistory}>
+    <Route path="/" component={App} >
+      <Route path="/item" component={SingleItem}/>
+    </Route>
+  </Router>
+, document.getElementById('root'));
